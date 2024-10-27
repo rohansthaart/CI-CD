7549f1408f7a103a5e7faca596bfbec8436ab571
rohansthaart <rohan-stha@live.com>
2024-10-27 18:49:35 +0545

    added comment in button

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,6 +12,6 @@ export const Button = ({ onClick, children }: ButtonProps) => {
     <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
       {children}
     </button>
-
+// added comment in buttom
   );
 };
